29dae0bd3dce5be98fa6ac1704713721ddd4221c
Shakil-Ahmmed <Shakilahmmed8882@gmail.com>
2023-10-09 06:18:25 +0600

    Animated Home page using Aos

--- src/Pages/🏠Home/Home.jsx
+++ src/Pages/🏠Home/Home.jsx
@@ -1,20 +1,33 @@
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 import Footer from "../Footer/Footer";
 import Banner from "./Banner/Banner";
 import Courses from "./Courses/Courses";
 import FAQ from "./FAQ";
 import WhyToChooseUs from "./WhyToChooseUs";
-
+import { useEffect } from "react";
 
 const Home = () => {
-      return (
-            <div className="space-y-11">
-                        <Banner></Banner>
-                        <Courses></Courses>
-                        <WhyToChooseUs></WhyToChooseUs>
-                        <FAQ></FAQ>
-                        <Footer></Footer>
-            </div>
-      );
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
+  return (
+    <div className="space-y-11">
+      <div data-aos="fade-left">
+        <Banner></Banner>
+      </div>
+      <Courses></Courses>
+      <div data-aos="fade-right">
+        <WhyToChooseUs></WhyToChooseUs>
+      </div>
+      <div data-aos="fade-up">
+        <FAQ></FAQ>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Home;
